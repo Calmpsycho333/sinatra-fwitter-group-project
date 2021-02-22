57d22d118366863028580e06e226b466437d3eb3
calmpsycho333 <wrathking75@yahoo.com>
2021-02-21 20:59:13 -0500

    commit tree 1dbbb23fa3f3c058ece7a2119082a589adb400f5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,28 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do
+    "Welcome to Fwitter!"
+  end
+
+  get '/signup' do
+
+    "Signup Page"
+
+  end
+
+  post '/signup' do 
+ 
+    unless params[:username].empty?
+      redirect "/tweets" 
+    end
+
+    end
 
 end
